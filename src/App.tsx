df40da543d07ff1d199a3c982270cfa3ddc025b5
benndzamin <mujka_benjo@hotmail.com>
2025-05-03 07:50:56 +0200

    vercele deploy - fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="top-0 left-0 min-h-screen bg-fixed bg-center bg-cover" style={{ backgroundImage: "url('/src/assets/bg-photo-listing.jpg')" }}>
