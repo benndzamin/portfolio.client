099e0b1f17bba8d13951c6553a7f171021351d09
benndzamin <mujka_benjo@hotmail.com>
2025-05-31 12:00:59 +0200

    my expertise-fix1

--- src/components/ExpertiseCard.tsx
+++ src/components/ExpertiseCard.tsx
@@ -16,7 +16,7 @@ const ExpertiseCard = ({
   return (
     <div className="border border-white/80 backdrop-blur-sm text-white p-6 w-full sm:w-[300px] transition duration-300 shadow-md relative">
       <span className="absolute top-5 left-6 m-2 text-[2.8rem]">{icon}</span>
-      <span className="text-xl font-bold text-white max-w-40 flex flex-col items-center ml-16 mb-2">
+      <span className="text-xl font-bold text-white min-h-14 flex flex-col items-center justify-center ml-16 mb-2">
         {title}
       </span>
       <p className="text-sm text-blue-400 font-medium">{subtitle}</p>
